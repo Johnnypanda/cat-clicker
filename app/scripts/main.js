6bb10e6312b15655e91d175992e3c473d2ed5196
Artem <afilinov93@gmail.com>
2018-04-11 18:40:39 +0200

    Completed click functionality, changed style

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,16 +7,33 @@
 		btns = document.querySelectorAll('li'),
 		displayClicks = document.querySelector('.click-num');
 
-
+	//Basic Cat constructor
 	let Cat = function(name, pic){
 		this.name = name;
 		this.pic = pic;
 		this.clicks = 0;
-		this.showCat = function(){
+	}
+	//Changes the displayed cat
+	Cat.prototype.showCat = function(){
 			console.log(this.pic);
+			//Sets and image src to current cat's pic
 			img.setAttribute('src', this.pic);
+			//Sets the displayed name to the name of the current cat
 			displayName.innerHTML = this.name;
+			//Sets the displayed clicks to that of the current cat
+			this.clicks = 0;
 			displayClicks.innerHTML = this.clicks;
+	}
+
+	//Method to increase num of clicks
+	Cat.prototype.addClick = function() {
+		  this.clicks++;
+		  displayClicks.innerHTML = this.clicks;
+	};
+	//Function that checks the current cat and increments it's clicks bumber
+	Cat.prototype.checkCat = function(){
+		if(this.name === displayName.innerHTML){
+			this.addClick();
 		}
 	}
 
@@ -28,6 +45,7 @@
 	let cat4 = new Cat('Pawson', 'images/cat4.jpg');
 	let cat5 = new Cat('Simon', 'images/cat5.jpg');
 
+	//Event listeners to display cat
 	btns[0].addEventListener('click', function(){
 		cat1.showCat();
 	});
@@ -42,4 +60,12 @@
 	});
 	btns[4].addEventListener('click', function(){
 		cat5.showCat();
+	});
+	//Event listener and function to display num of clicks
+	img.addEventListener('click', function(){
+		cat1.checkCat();
+		cat2.checkCat();
+		cat3.checkCat();
+		cat4.checkCat();
+		cat5.checkCat();
 	});
